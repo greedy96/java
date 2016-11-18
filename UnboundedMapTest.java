75be00ebb07a3beacf70e1260f452b8f3c254054
greedy96 <navix9@wp.pl>
2016-11-18 15:19:55 +0100

    Add files via upload

---
+++ UnboundedMapTest.java
@@ -0,0 +1,37 @@
+package agh.cs.lab5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import agh.cs.lab4.Car;
+
+public class UnboundedMapTest {
+
+	 @Test
+	    public void canMoveTo() throws Exception {
+		 	
+	    }
+
+	    @Test
+	    public void add() throws Exception {
+
+	    }
+
+	    @Test
+	    public void run() throws Exception {
+	    }
+
+	    @Test
+	    public void isOccupied() throws Exception {
+
+	    }
+
+	    @Test
+	    public void objectAt() throws Exception {
+
+	    }
+
+	}
+
+}
